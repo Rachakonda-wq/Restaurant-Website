32663f0e33725a973c6f25cf0179eeb1f7a72258
Rachakonda Pavan Kumar <rachakondapavanrao07@gmail.com>
2025-11-09 16:57:52 +0530

    Add files via upload

---
+++ jss.js
@@ -0,0 +1,21 @@
+// let newBtn=document.createElement("button");
+// newBtn.innerText="clickme!";
+// console.log(newBtn);
+
+// let div=document.querySelector("p");
+// div.after(newBtn);
+// let newheading=document.createElement("h1");
+// newheading.innerHTML="<i>Iam new!</i>";
+// document.querySelector("body").prepend(newheading);
+//  let para=document.querySelector("p")
+//  para.remove();
+// let newBtn=document.createElement("button")
+// newBtn.innerText="clickme";
+// console.log(newBtn);
+
+// let div=document.querySelector("body");
+// div.prepend(newBtn);
+// newBtn.style.backgroundColor="red";
+// newBtn.style.color="white";
+let para=document.querySelector("p")
+console.log(para)
